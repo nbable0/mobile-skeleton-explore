b8c5f452283955e74cb7ab6876ef1e52de447544
nick <nbable0@gmail.com>
2020-03-11 15:24:04 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+  const colorsExamples = anime.timeline({
+    endDelay: 40,
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true,
+    delay: anime.stagger(8, {easing: 'easeOutQuad'})
+  })
+  .add({ targets: '.skeleton',  background: ['#F8F8F8'] }, 0)
+      
+  //#38A4FF
